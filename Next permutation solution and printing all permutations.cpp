90127409feba9cee375110e22bb63248cf2864ba
Abhiram k r <109401380+abhiram002@users.noreply.github.com>
2023-10-10 01:57:54 +0530

    Next permutation solution and printing all permutations solution.cpp

---
+++ Next permutation solution and printing all permutations.cpp
@@ -0,0 +1,54 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> nextGreaterPermutation(vector<int> &A) {
+    int n = A.size(); // size of the array.
+
+    // Step 1: Find the break point:
+    int ind = -1; // break point
+    for (int i = n - 2; i >= 0; i--) {
+        if (A[i] < A[i + 1]) {
+            // index i is the break point
+            ind = i;
+            break;
+        }
+    }
+
+    // If break point does not exist:
+    if (ind == -1) {
+        // reverse the whole array:
+        reverse(A.begin(), A.end());
+        return A;
+    }
+
+    // Step 2: Find the next greater element
+    //         and swap it with arr[ind]:
+
+    for (int i = n - 1; i > ind; i--) {
+        if (A[i] > A[ind]) {
+            swap(A[i], A[ind]);
+            break;
+        }
+    }
+
+    // Step 3: reverse the right half:
+    reverse(A.begin() + ind + 1, A.end());
+
+    return A;
+}
+
+int main()
+{
+    vector<int> A = {3,1,8};
+    for(int i=0;i<6;i++){   // printing all permutations of 318...if we remove this we will gwt only the next permutation
+    vector<int> ans = nextGreaterPermutation(A);
+
+    cout << "The next permutation is: [";
+    for (auto it : ans) {
+        cout << it << " ";
+    }
+    cout << "]"<<endl;
+    }
+    return 0;
+}
